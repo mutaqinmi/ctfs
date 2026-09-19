9ee469442b0df38d2610f7a27959572a7e2aa557
mutaqinmi <mutaqinmi2586@gmail.com>
2026-09-19 21:17:25 +0700

    feat: S3 initialization

---
+++ src/lib/s3/index.ts
@@ -0,0 +1,15 @@
+import { env } from "$env/dynamic/private";
+import { S3Client } from "@aws-sdk/client-s3";
+
+if(!env.R2_ACCOUNT_ID || !env.R2_ACCESS_KEY_ID || !env.R2_SECRET_ACCESS_KEY) {
+    throw new Error('R2_ACCOUNT_ID, R2_ACCESS_KEY_ID, or R2_SECRET_ACCESS_KEY is not set');
+}
+
+export const s3 = new S3Client({
+	region: 'auto',
+	endpoint: `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com/${env.R2_BUCKET_NAME}`,
+	credentials: {
+		accessKeyId: env.R2_ACCESS_KEY_ID,
+		secretAccessKey: env.R2_SECRET_ACCESS_KEY
+	}
+});
